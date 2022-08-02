d3db61ed18979cf71f8d1777596fde0b7ac2e8de
ma-abdallah94 <85710599+ma-abdallah94@users.noreply.github.com>
2022-08-02 02:00:29 +0200

    No

--- src/test/java/findElement/findBy.java
+++ src/test/java/findElement/findBy.java
@@ -4,15 +4,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+@Test
 public class findBy {
 
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("file://C:\\Users\\maabd\\IdeaProjects\\QA_Selenium\\src\\test\\resources\\index.html");
-        String shit = driver.findElement(By.id("welcome")).getText();
-        System.out.println(shit);
+        driver.findElement(By.id("welcome"));
         driver.close();
 
     }
