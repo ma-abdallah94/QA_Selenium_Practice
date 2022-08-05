409be6731ac1694c0688563a3207ff413de83d3d
ma-abdallah94 <85710599+ma-abdallah94@users.noreply.github.com>
2022-08-05 02:36:55 +0200

    2 AM

--- src/test/java/findElement/findByXPATH.java
+++ src/test/java/findElement/findByXPATH.java
@@ -4,14 +4,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class findByCLASS {
+public class findByXPATH {
 
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("file://C:\\Users\\maabd\\IdeaProjects\\QA_Selenium\\src\\test\\resources\\index.html");
-        String testClass = driver.findElement(By.linkText("web")).getText();
+        String testClass = driver.findElement(By.xpath("//ul[@class='country-list']/li[2]")).getText();
+        // String testClass = driver.findElement(By.xpath("//html/body/ul[2]/li[2]")).getText();
         System.out.println(testClass);
         driver.close();
 
